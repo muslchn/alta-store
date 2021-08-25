ffbff2aaea1019b5c4fccbad74707d75f347795b
Muslichin <muslichin.ach@gmail.com>
2021-08-25 15:23:59 +0700

    Create config

---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	config := map[string]string{
+		"DB_Username": "root",
+		"DB_Password": "123ABC4d.",
+		"DB_Port":     "3306",
+		"DB_Host":     "localhost",
+		"DB_Name":     "alta",
+	}
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		config["DB_Username"],
+		config["DB_Password"],
+		config["DB_Host"],
+		config["DB_Port"],
+		config["DB_Name"],
+	)
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
