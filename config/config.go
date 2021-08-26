bd524cb3a98d48379f8be83810b1d5bae088eeb9
Muslichin <muslichin.ach@gmail.com>
2021-08-26 19:40:08 +0700

    Uninitiate data to database

--- config/config.go
+++ config/config.go
@@ -35,125 +35,127 @@ func InitDB() {
 
 	InitMigrate()
 
-	DB.Create(&models.Category{
-		ID:   1,
-		Name: "Buku",
-	})
+	/*
+		DB.Create(&models.Category{
+			ID:   1,
+			Name: "Buku",
+		})
 
-	DB.Create(&models.Category{
-		ID:   2,
-		Name: "Handphone",
-	})
+		DB.Create(&models.Category{
+			ID:   2,
+			Name: "Handphone",
+		})
 
-	DB.Create(&models.Category{
-		ID:   3,
-		Name: "Laptop",
-	})
+		DB.Create(&models.Category{
+			ID:   3,
+			Name: "Laptop",
+		})
 
-	DB.Create(&models.Product{
-		ID:          11,
-		CategoryID:  1,
-		Name:        "Sapiens",
-		Description: "Riwayat Singkat Umat Manusia ...",
-		Stock:       3,
-		Price:       115000,
-		Weight:      700,
-	})
+		DB.Create(&models.Product{
+			ID:          11,
+			CategoryID:  1,
+			Name:        "Sapiens",
+			Description: "Riwayat Singkat Umat Manusia ...",
+			Stock:       3,
+			Price:       115000,
+			Weight:      700,
+		})
 
-	DB.Create(&models.Product{
-		ID:          12,
-		CategoryID:  1,
-		Name:        "Disruption",
-		Description: "Disruptive Innovation ...",
-		Stock:       3,
-		Price:       116900,
-		Weight:      500,
-	})
+		DB.Create(&models.Product{
+			ID:          12,
+			CategoryID:  1,
+			Name:        "Disruption",
+			Description: "Disruptive Innovation ...",
+			Stock:       3,
+			Price:       116900,
+			Weight:      500,
+		})
 
-	DB.Create(&models.Product{
-		ID:          13,
-		CategoryID:  1,
-		Name:        "Atomic Habits",
-		Description: "Perubahan Kecil Yang Memberikan Hasil Luar Biasa ...",
-		Stock:       3,
-		Price:       86500,
-		Weight:      500,
-	})
+		DB.Create(&models.Product{
+			ID:          13,
+			CategoryID:  1,
+			Name:        "Atomic Habits",
+			Description: "Perubahan Kecil Yang Memberikan Hasil Luar Biasa ...",
+			Stock:       3,
+			Price:       86500,
+			Weight:      500,
+		})
 
-	DB.Create(&models.Product{
-		ID:          21,
-		CategoryID:  2,
-		Name:        "iPhone 12 128GB",
-		Description: "Garansi Resmi iBox Apple Indonesia ...",
-		Stock:       3,
-		Price:       13950000,
-		Weight:      500,
-	})
+		DB.Create(&models.Product{
+			ID:          21,
+			CategoryID:  2,
+			Name:        "iPhone 12 128GB",
+			Description: "Garansi Resmi iBox Apple Indonesia ...",
+			Stock:       3,
+			Price:       13950000,
+			Weight:      500,
+		})
 
-	DB.Create(&models.Product{
-		ID:          22,
-		CategoryID:  2,
-		Name:        "Samsung Galaxy A12 128GB",
-		Description: "PLATFORM OS Android 10 ...",
-		Stock:       3,
-		Price:       2254000,
-		Weight:      500,
-	})
+		DB.Create(&models.Product{
+			ID:          22,
+			CategoryID:  2,
+			Name:        "Samsung Galaxy A12 128GB",
+			Description: "PLATFORM OS Android 10 ...",
+			Stock:       3,
+			Price:       2254000,
+			Weight:      500,
+		})
 
-	DB.Create(&models.Product{
-		ID:          23,
-		CategoryID:  2,
-		Name:        "Oppo Reno6 128GB",
-		Description: "Garansi Resmi Oppo 1 Tahun ...",
-		Stock:       3,
-		Price:       4405000,
-		Weight:      500,
-	})
+		DB.Create(&models.Product{
+			ID:          23,
+			CategoryID:  2,
+			Name:        "Oppo Reno6 128GB",
+			Description: "Garansi Resmi Oppo 1 Tahun ...",
+			Stock:       3,
+			Price:       4405000,
+			Weight:      500,
+		})
 
-	DB.Create(&models.Product{
-		ID:          31,
-		CategoryID:  3,
-		Name:        "MacBook Air M1",
-		Description: "Macbook Terbaik ...",
-		Stock:       3,
-		Price:       15990000,
-		Weight:      5000,
-	})
+		DB.Create(&models.Product{
+			ID:          31,
+			CategoryID:  3,
+			Name:        "MacBook Air M1",
+			Description: "Macbook Terbaik ...",
+			Stock:       3,
+			Price:       15990000,
+			Weight:      5000,
+		})
 
-	DB.Create(&models.Product{
-		ID:          32,
-		CategoryID:  3,
-		Name:        "ASUS ROG Zephyrus G15",
-		Description: "Laptop Gaming Terbaik ...",
-		Stock:       3,
-		Price:       32999000,
-		Weight:      6000,
-	})
+		DB.Create(&models.Product{
+			ID:          32,
+			CategoryID:  3,
+			Name:        "ASUS ROG Zephyrus G15",
+			Description: "Laptop Gaming Terbaik ...",
+			Stock:       3,
+			Price:       32999000,
+			Weight:      6000,
+		})
 
-	DB.Create(&models.Product{
-		ID:          33,
-		CategoryID:  3,
-		Name:        "Acer Swift 3 Infinity 4",
-		Description: "Laptop untuk Mahasiswa ...",
-		Stock:       3,
-		Price:       12499000,
-		Weight:      5000,
-	})
+		DB.Create(&models.Product{
+			ID:          33,
+			CategoryID:  3,
+			Name:        "Acer Swift 3 Infinity 4",
+			Description: "Laptop untuk Mahasiswa ...",
+			Stock:       3,
+			Price:       12499000,
+			Weight:      5000,
+		})
 
-	DB.Create(&models.Payment{
-		ID:   1,
-		Type: "LinkAja",
-	})
+		DB.Create(&models.Payment{
+			ID:   1,
+			Type: "LinkAja",
+		})
 
-	DB.Create(&models.Payment{
-		ID:   2,
-		Type: "OVO",
-	})
+		DB.Create(&models.Payment{
+			ID:   2,
+			Type: "OVO",
+		})
 
-	DB.Create(&models.Payment{
-		ID:   3,
-		Type: "GoPay",
-	})
+		DB.Create(&models.Payment{
+			ID:   3,
+			Type: "GoPay",
+		})
+	*/
 }
 
 func InitMigrate() {
