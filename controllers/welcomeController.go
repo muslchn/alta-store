c40d00f91d21d0c24d804cab8cfdcd485535d94b
Muslichin <muslichin.ach@gmail.com>
2021-08-31 08:53:31 +0700

    Create welcome controller

---
+++ controllers/welcomeController.go
@@ -0,0 +1,11 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func WelcomeController(c echo.Context) error {
+	return c.String(http.StatusOK, "Welcome to ALTA STORE")
+}
