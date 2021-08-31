cccd824abbe182b99251725350fce2f077593194
Muslichin <muslichin.ach@gmail.com>
2021-08-31 07:53:30 +0700

    Create login customers controller

--- controllers/customerController.go
+++ controllers/customerController.go
@@ -45,16 +45,16 @@ func RegisterController(c echo.Context) error {
 	})
 }
 
-// type Customer struct {
-// 	gorm.Model
-// 	FirstName  string `gorm:"size:255;not null" json:"firstName" form:"firstName"`
-// 	LastName   string `gorm:"size:255" json:"lastName" form:"lastName"`
-// 	Username   string `gorm:"size:255;not null;unique" json:"username" form:"username"`
-// 	Email      string `gorm:"size:100;not null;unique" json:"email" form:"email"`
-// 	Password   string `gorm:"size:100;not null" json:"password" form:"password"`
-// 	Phone      string `gorm:"size:15;not null" json:"phone" form:"phone"`
-// 	Address    string `gorm:"size:255;not null" json:"address" form:"address"`
-// 	City       string `gorm:"size:100;not null" json:"city" form:"city"`
-// 	State      string `gorm:"size:100;not null" json:"state" form:"state"`
-// 	PostalCode string `gorm:"5;not null" json:"postalCode" form:"postalCode"`
-// }
+func LoginCustomersController(c echo.Context) error {
+	customer := models.Customer{}
+	c.Bind(&customer)
+
+	customers, err := database.LoginCustomers(&customer)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status":    "success login",
+		"customers": customers,
+	})
+}
