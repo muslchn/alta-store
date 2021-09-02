b0b2c6307752ce8709a1f0c8f6c962abe506cd4f
Muslichin <muslichin.ach@gmail.com>
2021-09-02 18:46:02 +0700

    Create get category controller

--- controllers/categoryControllers.go
+++ controllers/categoryControllers.go
@@ -1,1 +1,21 @@
 package controllers
+
+import (
+	"alta-store/lib/database"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func GetCategoryController(c echo.Context) error {
+	category, err := database.GetCategory()
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status":   "success",
+		"category": category,
+	})
+}
