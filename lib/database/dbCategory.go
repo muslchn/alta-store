0945841461ec4496cc33671246f212465fbc03e4
Muslichin <muslichin.ach@gmail.com>
2021-09-02 17:40:43 +0700

    Create get category database

--- lib/database/dbCategory.go
+++ lib/database/dbCategory.go
@@ -1,1 +1,16 @@
 package database
+
+import (
+	"alta-store/config"
+	"alta-store/models"
+)
+
+func GetCategory() (interface{}, error) {
+	var category []models.Category
+
+	if err := config.DB.Find(&category).Error; err != nil {
+		return nil, err
+	}
+
+	return category, nil
+}
