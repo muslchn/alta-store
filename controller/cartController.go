1f36d09951e3730e66868ce2a7ce9de17ade6726
Muslichin <muslichin.ach@gmail.com>
2021-09-05 20:50:00 +0700

    Create delete cart item controller

--- controller/cartController.go
+++ controller/cartController.go
@@ -4,23 +4,10 @@ import (
 	"alta-store/lib/database"
 	"alta-store/middleware"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
-
-func GetCartController(c echo.Context) error {
-	customerId := middleware.ExtractTokenCustomerId(c)
-	cart, err := database.GetCart(uint(customerId))
-
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
-
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"status":   "success",
-		"products": cart,
-	})
-}
 
 func AddCartItemController(c echo.Context) error {
 	customerId := middleware.ExtractTokenCustomerId(c)
@@ -36,5 +23,44 @@ func AddCartItemController(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"status": "success",
 		"item":   cartItem,
+	})
+}
+
+func GetCartController(c echo.Context) error {
+	customerId := middleware.ExtractTokenCustomerId(c)
+	cart, err := database.GetCart(uint(customerId))
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status":   "success",
+		"products": cart,
+	})
+}
+
+func DeleteCartItemController(c echo.Context) error {
+	customerId := middleware.ExtractTokenCustomerId(c)
+	cartItemId, _ := strconv.Atoi(c.Param("id"))
+	validate, validateItem, err := database.ValidateCartItem(uint(cartItemId), uint(customerId))
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if !validate {
+		return c.JSON(http.StatusForbidden, map[string]string{
+			"status":  "fail",
+			"message": "You do not have access to delete the data",
+		})
+	}
+
+	database.ReturnStock(validateItem["product_id"], validateItem["qty"])
+	database.DeleteCartItem(uint(cartItemId))
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"status":  "success",
+		"message": "Product has been removed from the cart",
 	})
 }
