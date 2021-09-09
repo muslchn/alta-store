2020c863f973619b99505f99455c11d9eff7d24f
Muslichin <muslichin.ach@gmail.com>
2021-09-09 10:11:10 +0700

    Edit payment model

--- controller/paymentController.go
+++ controller/paymentController.go
@@ -3,6 +3,7 @@ package controller
 import "github.com/labstack/echo/v4"
 
 func PaymentController(c echo.Context) error {
+
 	return nil
 }
 
--- model/payment.go
+++ model/payment.go
@@ -1,16 +1,12 @@
 package model
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Payment struct {
 	gorm.Model
-	ID      uint `gorm:"primaryKey"`
-	Type    string
-	Allowed bool
-	Time    time.Time
-	Paid    bool
+	CheckoutID uint
+	Paid       uint
+	Checkout   Checkout
 }
