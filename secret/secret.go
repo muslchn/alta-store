b6dc3cecab160d004ca7ec9ae3f56b492208e692
Muslichin <muslichin.ach@gmail.com>
2021-09-11 18:38:32 +0700

    Edit .gitignore

---
+++ secret/secret.go
@@ -0,0 +1,11 @@
+package secret
+
+const (
+	DB_USERNAME = "root"
+	DB_PASSWORD = "123ABC4d."
+	DB_PORT     = "3306"
+	DB_HOST     = "localhost"
+	DB_NAME     = "alta-store"
+
+	SECRET_JWT = "tahubulat63"
+)
